b1714e8980e5beb19d00f370a40706d551db4876
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-07-29 09:16:55 -0300

    Criando as factories para popular banco de dados e ajustando models

--- database/factories/ClienteFactory.php
+++ database/factories/ClienteFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,7 @@ class ClienteFactory extends Factory
     public function definition(): array
     {
         return [
-            'usuario_id' => \App\Models\User::factory(),
+            'user_id' => User::factory(),
             'cliente_nome' => $this->faker->name()
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cliente;
+use App\Models\Empresa;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Empresa::factory(5)->create();
+        User::factory(10)->create();
+        Cliente::factory(30)->create();
     }
 }
